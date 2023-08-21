b5973134808283ad91147344e56a8e5afdaf99c9
PhMachado-ctrl <phmachado555@gmail.com>
2023-08-21 09:27:22 -0300

    Ola react

---
+++ todo/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function App() {
+  //Todos: COnsulta os dados setTodos: Insere os dados
+  const [Todos, setTodos] = useState()
+  return <div>Olá React</div>;
+}
+
+export default App
