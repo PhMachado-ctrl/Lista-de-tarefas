45a5699655c5418dfd2bd8aeba3d29410eb2bb05
PhMachado-ctrl <phmachado555@gmail.com>
2023-08-21 10:28:55 -0300

    text-category

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,9 +1,42 @@
 import { useState } from 'react'
 
 function App() {
-  //Todos: COnsulta os dados setTodos: Insere os dados
-  const [Todos, setTodos] = useState()
-  return <div>Olá React</div>;
+  //Todos: Consulta os dados | setTodos: Insere os dados
+  const [Todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+  return <div className='app'>
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {Todos.map((todo) => (
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className="category">
+              {todo.category}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>;
 }
 
 export default App
