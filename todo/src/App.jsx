2880b4ed07e41b1f6c2a231d32cb66cce1273b8e
PhMachado-ctrl <phmachado555@gmail.com>
2023-08-25 17:09:39 -0300

    remove-todo

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -28,26 +28,37 @@ function App() {
     },
   ]);
 
-const addTodo = (text, category) => {
-    const newTodos = [...todos, {
-      id: Math.floor(Math.random() * 10000),
-      text,
-      category,
-      isCompleted: false,
-    },
-  ];
-  setTodos(newTodos);
-};
+  const addTodo = (text, category) => {
+      const newTodos = [...todos, {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+    setTodos(newTodos);
+  };
 
-  return <div className='app'>
-    <h1>Lista de Tarefas</h1>
-    <div className="todo-list">
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo = {todo} />
-      ))}
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) => 
+      todo.id !== id ? todo : null 
+    );
+    setTodos(filteredTodos);
+  }
+
+
+  return (
+    <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo = {todo} removeTodo={removeTodo} />
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
     </div>
-    <TodoForm addTodo={addTodo} />
-  </div>;
+  );
 }
 
 export default App
